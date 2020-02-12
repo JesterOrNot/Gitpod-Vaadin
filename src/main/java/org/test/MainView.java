9e79c5a239abf6ee84a8b7d57aeeb83d8f511299
Sean Hellum <seanhellum45@gmail.com>
2020-02-12 21:15:53 +0000

    Update App

--- src/main/java/org/test/MainView.java
+++ src/main/java/org/test/MainView.java
@@ -1,6 +1,8 @@
 package org.test;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.server.PWA;
@@ -14,8 +16,22 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
+        TextField nameField = new TextField("Name");
+        TextField ageField = new TextField("Age");
+        Paragraph greeting = new Paragraph("");
+        Button myButton = new Button("Button!", event -> {
+            int num;
+            try {
+                num = Integer.parseInt(String.valueOf(ageField.getValue()));
+            } catch (NumberFormatException e) {
+                num = 0;
+                Notification.show("Please enter a valid number");
+            }
+            String canVote = num >= 18 ? " Can Vote" : " Can't Vote";
+            greeting.setText("Hello, " + nameField.getValue() + canVote);
+            nameField.clear();
+            ageField.clear();
+        });
+        add(nameField, ageField, greeting, myButton);
     }
 }
